86a73cfd0fbafc4314d5af5b2d8fc7222a9dfc52
Andy Korth <andy@howlingmoonsoftware.com>
2017-01-21 12:46:57 -0600

    Support for Vive + another display

---
+++ Assets/MultipleDisplayManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleDisplayManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int displayCount = 0;
+		foreach (Display d in Display.displays) {
+			displayCount += 1;
+			d.Activate ();
+		}
+		Debug.Log ("Active displays: " + displayCount);
+		if (displayCount <= 1) {
+			Debug.LogError ("EarthBeat requires a vive display and a secondary display. It won't work well!");
+		}
+	}
+	
+
+}
