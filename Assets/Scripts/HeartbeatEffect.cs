c100366736d08720cc9eb41fd5433e55d74b575e
Caleb Biasco <biasc007@umn.edu>
2017-01-21 20:01:57 -0600

    Animated heartbeat sphere

---
+++ Assets/Scripts/HeartbeatEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartbeatEffect : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = transform.localScale + Vector3.one;
+
+		if (transform.localScale.x > 70f)
+			Destroy(gameObject);
+	}
+}
