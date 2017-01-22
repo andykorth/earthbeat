7eaa5b4b08714d823ff9088e0ac8119b90ac5cf0
Caleb Biasco <biasc007@umn.edu>
2017-01-21 23:28:26 -0600

    Added despawn time to destroyed crates

---
+++ Assets/CrateDespawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateDespawner : MonoBehaviour {
+
+	private float despawnTime = 5f;
+	//private Transform[] children;
+
+	void Start()
+	{
+		//children = gameObject.GetComponentsInChildren<Transform>(true);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		despawnTime -= Time.deltaTime;
+		if (despawnTime < 0)
+		{
+			gameObject.transform.localScale -= Vector3.one * .015f;
+			if (gameObject.transform.localScale.x <= 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+		
+		// Ineffective attempt at shrinking individual pieces
+		/*if (despawnTime < 0)
+		{
+			foreach (Transform child in children)
+			{
+				child.localScale -= Vector3.one * .01f;
+				if (child.localScale.x <= 0)
+				{
+					Destroy(child.gameObject);
+				}
+			}
+
+			if (children.Length == 0)
+				Destroy(gameObject);
+		}*/
+	}
+}
