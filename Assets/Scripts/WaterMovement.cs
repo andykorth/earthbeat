d9ded9d640e4993ca7f82c47078e2321c1843064
Nathan <scvnathan@gmail.com>
2017-01-26 00:52:12 -0600

    Updated water effects

---
+++ Assets/Scripts/WaterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour {
+    private MeshRenderer _renderer;
+    void Start() {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+	void Update () {
+	    var offset = _renderer.material.mainTextureOffset;
+	    _renderer.material.mainTextureOffset = new Vector2(
+	        Mathf.Lerp(offset.x, offset.x + 0.002f, Time.time),
+	        0f
+	    );
+
+	    var y = Mathf.Lerp(transform.position.y, transform.position.y + Mathf.Sin(Time.time)/4, Time.deltaTime);
+	    var tidePos = new Vector3(transform.position.x, y, transform.position.z);
+	    Debug.Log(tidePos);
+	    transform.position = tidePos;
+	}
+}
